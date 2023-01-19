e3731bdc95b34e39b5810d17ac5a6e36978abd9c
ASyrotenko <andreysyrotenko@gmail.com>
2023-01-19 10:40:16 +0200

    add icon and remove contacts

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,12 +9,7 @@ import styles from './app.module.css';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: shortid.generate(), name: 'Rosie Simpson', number: '459-12-56' },
-      { id: shortid.generate(), name: 'Hermione Kline', number: '443-89-12' },
-      { id: shortid.generate(), name: 'Eden Clements', number: '645-17-79' },
-      { id: shortid.generate(), name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
